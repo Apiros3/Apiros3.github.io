a083fa102a1a60b1ab376b8ae1655c6a8cbe46f6
Apiros3 <akikan.contact@gmail.com>
2022-09-06 00:18:43 +0900

    adding template sentence

--- algo-qs/script.js
+++ algo-qs/script.js
@@ -1,11 +1,9 @@
 function problem() {
     document.getElementById("question").style.display = block;
     document.getElementById("answer").style.display = none;
-    
 }
 
 function answer() {
     document.getElementById("question").style.display = none;
     document.getElementById("answer").style.display = block;
-
 }
