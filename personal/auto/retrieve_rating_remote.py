6874ad7e54c3bd5c3315190414a4ab13ba3006eb
Apiros3 <akikan.contact@gmail.com>
2023-08-16 00:11:32 +0900

    added some automated rating retrievers for atc, cf, and omc

--- personal/auto/retrieve_rating_remote.py
+++ personal/auto/retrieve_rating_remote.py
@@ -2,12 +2,22 @@
 
 from urllib.request import urlopen
 import json
+import requests
+from bs4 import BeautifulSoup as bs
+import os
+
+print("retrieve_rating.py starting...")
 
 #!!!!! LOCAL ONLY !!!!!
-url = "https://clist.by:443/api/v3/statistics/?username=Apiros3&api_key=9ff90d500d596b7a40a62c515ec8e6dcacaf04c3&coder_id=25319&order_by=-date"
+url = os.environ["CLISTURL"]
 
 response = urlopen(url)
 data_json = json.loads(response.read())
+
+f = open('rating_list.txt', 'w')
+
+atcur = -1
+cfcur = -1
 
 for data in data_json["objects"]:
     if (data["new_rating"] == None):
@@ -21,18 +31,30 @@ for data in data_json["objects"]:
         if (ev[i] == "Codeforces"):
             ev = ev[i:i+3]
             break 
-    print(ev[0])
-    print(' '.join(ev))
+    if (ev[0] == "AtCoder" and atcur == -1):
+        atcur = data["new_rating"]
+    if (ev[0] == "Codeforces" and cfcur == -1):
+        cfcur = data["new_rating"]
+    f.writelines(f'{ev[0]}\n')
+    f.writelines(' '.join(ev) + '\n')
     #codeforces contests without a # implies div1&2
-    print(f'{data["new_rating"]} {data["rating_change"]} {data["date"]}')
+    f.writelines(f'{data["new_rating"]} {data["rating_change"]} {data["date"]}\n')
     #info to have: date, place, rating change, new rating, contest site (AtCoder/Codeforces)
 
+f.close()
+
+url = "https://onlinemathcontest.com/users/apiros3"
+res = requests.get(url)
+lis = bs(res.content, features="html.parser").find_all('p', {'id' : 'rating'})
+omccur = lis[0].text.split()[-1]
+
+f = open('current_rating.txt', 'w')
+f.writelines(f'{atcur}\n{cfcur}\n{omccur}')
 
 #use the given data above to also find, current rating for both AtCoder and Codeforces (simply the first instance)
+print("done!!")
 
 
-f = open('out.txt', 'w')
 
 
-f.writelines(str(data_json))
 
