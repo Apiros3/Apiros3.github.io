c47f26b09185b978fb4423428ab8b0f3fd375648
Apiros3 <akikan.contact@gmail.com>
2023-08-18 10:00:30 +0900

    testing actions

--- display_rating.js
+++
@@ -1,8 +0,0 @@
-
-function update_rating_once() { 
-    document.getElementById("rating_atcoder").innerHTML = 1261
-    document.getElementById("rating_codeforces").innerHTML = 1423
-    document.getElementById("rating_omc").innerHTML = 921
-    
-    document.getElementById("rating_last_update").innerHTML = "2023-08-18T00:58:02"
-} 
