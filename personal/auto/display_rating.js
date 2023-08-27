36c944ecc8f5cb5595007c171dbd8e9482bdc1fc
GitHub Action <github-actions@github.com>
2023-08-27 01:19:30 +0000

    new rating

--- personal/auto/display_rating.js
+++ personal/auto/display_rating.js
@@ -4,5 +4,5 @@ function update_rating_once() {
     document.getElementById("rating_codeforces").innerHTML = 1663
     document.getElementById("rating_omc").innerHTML = 921
     
-    document.getElementById("rating_last_update").innerHTML = "2023-08-26T01:12:43"
+    document.getElementById("rating_last_update").innerHTML = "2023-08-27T01:19:30"
 } 
