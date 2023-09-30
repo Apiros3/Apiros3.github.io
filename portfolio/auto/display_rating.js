deb71cc63337dd36d3ca691f3b8fed01724108bf
GitHub Action <github-actions@github.com>
2023-09-30 01:15:33 +0000

    new rating

--- portfolio/auto/display_rating.js
+++ portfolio/auto/display_rating.js
@@ -4,5 +4,5 @@ function update_rating_once() {
     document.getElementById("rating_codeforces").innerHTML = 1663
     document.getElementById("rating_omc").innerHTML = 921
     
-    document.getElementById("rating_last_update").innerHTML = "2023-09-29T01:17:09"
+    document.getElementById("rating_last_update").innerHTML = "2023-09-30T01:15:33"
 } 
