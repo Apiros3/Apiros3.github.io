8fc1be7a2dd0f8ef5e1c728a4ebe3bef6612af53
Apiros3 <lemon211k04@gmail.com>
2024-04-20 17:07:11 +0100

    updated mainpage

--- script.js
+++ script.js
@@ -9,9 +9,8 @@ Order: Date, content
 */
 
 events = [
-    ["Date:","Content:"],
-    ["Apr 12 2024", 'I participated in <a href="https://www.cs.le.ac.uk/events/mgs2024/">Midlands Graduate School (MGS) 2024</a>, taking Category Theory, Proof Theory, Type Theory, and Session Types courses'],
-    ["Feb 25 2024", 'The OEIS entry <a href="https://oeis.org/A369580">A369580</a> was approved and is now public.']
+    ["Apr 12 2024", 'I participated in <a href="https://www.cs.le.ac.uk/events/mgs2024/">Midlands Graduate School (MGS) 2024</a> hosted at the University of Leicester, taking Category Theory, Proof Theory, Type Theory, and Session Types courses.'],
+    ["Feb 25 2024", 'The OEIS entry <a href="https://oeis.org/A369580">A369580</a> regarding a probability sequence on a coin flipping game was approved and is now public.']
 ]
 
 function add_recent_events() {
@@ -19,8 +18,7 @@ function add_recent_events() {
     var table = document.getElementById("recent_events")
     for(let i = 0; i < Math.min(events.length, max_event_count+1); i++) {
         var row = table.insertRow(i);
-        row.insertCell(0).innerHTML = events[i][0];
-        row.insertCell(1).innerHTML = events[i][1];
+        row.insertCell(0).innerHTML = `<div class="event_date">${events[i][0]}</div><div class="event_context">${events[i][1]}</div>`
     }
 }
 
