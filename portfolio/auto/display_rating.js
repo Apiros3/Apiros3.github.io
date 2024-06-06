dec3b21e0faf61d2436f9d67a4a9746bc23c2746
GitHub Action <github-actions@github.com>
2024-06-06 00:24:06 +0000

    new rating

--- portfolio/auto/display_rating.js
+++ portfolio/auto/display_rating.js
@@ -4,5 +4,5 @@ function update_rating_once() {
     document.getElementById("rating_codeforces").innerHTML = 1664
     document.getElementById("rating_omc").innerHTML = 1080
     
-    document.getElementById("rating_last_update").innerHTML = "2024-06-05T00:24:10"
+    document.getElementById("rating_last_update").innerHTML = "2024-06-06T00:24:05"
 } 
